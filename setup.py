4d1984abfa32cdfe55e3cd31cbad262980c283ec
Andrea Uher <andrea.uher@hudl.com>
2017-03-20 13:46:36 -0500

    Changed package name to reflect hull fork

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='behave-teamcity',
     version="0.1.24",
-    packages=['behave_teamcity', ],
+    packages=['hudl_behave_teamcity', ],
     url='https://github.com/iljabauer/behave-teamcity',
     download_url='https://github.com/iljabauer/behave-teamcity/releases/tag/0.1.23',
     license='MIT',
