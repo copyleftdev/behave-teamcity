d46468f384555d3b040d4a8be9650c9112a5187e
Andrea Uher <andrea.uher@hudl.com>
2017-03-20 20:48:36 -0500

    Updating setup file

--- setup.py
+++ setup.py
@@ -3,20 +3,11 @@ from distutils.core import setup
 setup(
     name='hudl_behave-teamcity',
     version="0.1.24",
-    packages=['hudl_behave_teamcity', ],
+    packages=['hudl_behave_teamcity'],
     url='https://github.com/hudl/behave-teamcity',
-    license='MIT',
     author='Ilja Bauer',
     author_email='i.bauer@cuescience.de',
     description='TeamCity test report formatter for behave',
     install_requires=["behave>=1.2.5,<=1.3", "teamcity-messages"],
-    keywords=['testing', 'behave', 'teamcity', 'formatter', 'report'],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Software Development :: Testing",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: Utilities"
-    ],
+    keywords=['testing', 'behave', 'teamcity', 'formatter', 'report']
 )
